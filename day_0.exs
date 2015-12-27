82dd30babfbd428cba1aa21c8f93a831c9e6a4f0
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-26 22:01:56 -0600

    Cleaning up a little

--- day_0.exs
+++ day_0.exs
@@ -1,12 +1,10 @@
-instructions = "((()))))"  # should result in -2
-
 defmodule InstructionReader do
   @up_char "("
   @down_char ")"
   def render_result(instructions) do
       res = 0
-      up = for <<"(" <- instructions>>, do: 1
-      down = for <<")" <- instructions>>, do: 1
+      up = for <<@up_char <- instructions>>, do: 1
+      down = for <<@down_char <- instructions>>, do: 1
       up = Enum.reduce up, 0, &(&1 + &2)
       down = Enum.reduce down, 0, &(&1 + &2)
       up - down

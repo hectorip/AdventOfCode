3819fc59fb89d8101083c42c2113f0c290052abb
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-26 21:57:58 -0600

    Day 0, second try

--- day_0.exs
+++ day_0.exs
@@ -5,9 +5,11 @@ defmodule InstructionReader do
   @down_char ")"
   def render_result(instructions) do
       res = 0
-      for <<"(" <- instructions>>, do: res = res + 1
-      for <<")" <- instructions>>, do: res = res - 1
-      res
+      up = for <<"(" <- instructions>>, do: 1
+      down = for <<")" <- instructions>>, do: 1
+      up = Enum.reduce up, 0, &(&1 + &2)
+      down = Enum.reduce down, 0, &(&1 + &2)
+      up - down
   end
 end
 
